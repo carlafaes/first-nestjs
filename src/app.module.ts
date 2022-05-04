6ae7740a1074d37e6201dc86f0fdc81f8799ec47
carlafaes <carlafaes18@hotmail.com>
2022-05-04 09:11:37 -0300

    mo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InitialModule } from './initial/initial.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [InitialModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
