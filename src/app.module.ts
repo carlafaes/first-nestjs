937932d8d3b61e1bbecbfc4c2ea626fd744b0bb9
carlafaes <carlafaes18@hotmail.com>
2022-05-04 10:34:35 -0300

    mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InitialModule } from './initial/initial.module';
 import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [InitialModule, ProductModule],
+  imports: [InitialModule, ProductModule, MongooseModule],
   controllers: [AppController],
   providers: [AppService],
 })
